9bb1d6f782632a4bd9706cb77b1a4f7690908322
shivant47@gmail.com <shivant47@gmail.com>
2020-11-05 22:18:37 +0530

    new commit

--- src/Atmcomplaint.kt
+++ src/Atmcomplaint.kt
@@ -1,12 +1,25 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+
 class Atmcomplaint {
 
     companion object
     {
 
         fun atm_complaint(){
+            println("Welcome to The Facility of Customer Grievence Cell")
 
-     println("welcome to grivence cell of customer unit")
+            Dateandtime.dateandtime()
 
+            val fileName = "src/resources/myfile3.txt"
+
+            val myfile = File(fileName)
+            val content = BufferedReader(InputStreamReader(System.`in`))
+
+            myfile.writeText(content.toString())
+
+            println("Compaint Registered Successfully!!")
 
 
 
