13dcb3f259274d969936ab63358c50bbdd9fa039
shivant47@gmail.com <shivant47@gmail.com>
2020-11-07 22:39:48 +0530

    api call

--- src/Interest_Calculator.kt
+++ src/Interest_Calculator.kt
@@ -8,22 +8,35 @@ class Interest_Calculator {
         fun SimpleInterest() {
 
             val scanner = Scanner(System.`in`)
-
-            print("Enter principal amount : ")
-            var p: Int = scanner.nextInt()
-
-            print("Enter rate of interest : ")
-            var r: Int = scanner.nextInt()
-
-            print("Enter number of years : ")
-            var n: Int = scanner.nextInt()
-
-            var SI: Int = (p * n * r) / 100
-            var total= SI + p
+             val ch=scanner.nextInt();
+            if (ch==1) {
 
 
-            println("Simple interest for Your Amount ${p} and Rate ${r} and Year ${n} as your Required is: " + SI)
-            println("After Adding Your Interest Your Amout will be: $total")
+                print("Enter principal amount : ")
+                var p: Int = scanner.nextInt()
+
+                print("Enter rate of interest : ")
+                var r: Int = scanner.nextInt()
+
+                print("Enter number of years : ")
+                var n: Int = scanner.nextInt()
+
+                var SI: Int = (p * n * r) / 100
+                var total = SI + p
+
+
+                println("Simple interest for Your Amount ${p} and Rate ${r} and Year ${n} as your Required is: " + SI)
+                println("After Adding Your Interest Your Amout will be: $total")
+            }
+            else if (ch == 2){
+
+                println("The current SI of the Bank is 10%  according to RBI rules of Interest")
+                println(" As per RBI rules of the guidelines, Inerest is not more than 0% of the corresponding banks")
+
+
+            }
+
+
 
 
         }
